3b98e5f35ea27692ea8d7110db5e54589d52d893
ShrutiKulkarni03 <shru3k@gmail.com>
2020-05-26 11:55:03 +0530

    Delete

--- PractiseCodes/03-Hexagon/Hexagon.bat
+++
@@ -1,8 +0,0 @@
-REM Compile
-cl.exe /c /EHsc /I C:\freeglut\include Hexagon.cpp
-
-REM Link
-link.exe Hexagon.obj /LIBPATH:C:\freeglut\lib /SUBSYSTEM:CONSOLE
-
-REM Execute
-Hexagon.exe
--- PractiseCodes/04-Square/Square.bat
+++
@@ -1,8 +0,0 @@
-REM Compile
-cl.exe /c /EHsc /I C:\freeglut\include Square_with_Triangles.cpp
-
-REM Link
-link.exe Square_with_Triangles.obj /LIBPATH:C:\freeglut\lib /SUBSYSTEM:CONSOLE
-
-REM Execute
-Square_with_Triangles.exe
