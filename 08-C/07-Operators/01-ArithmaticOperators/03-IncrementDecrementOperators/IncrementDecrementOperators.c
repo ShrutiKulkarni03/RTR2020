bf81f6a1577059857cff1ff2c644f900d798a429
ShrutiKulkarni03 <shru3k@gmail.com>
2020-05-29 14:18:14 +0530

    C Snippets

--- 08-C/07-Operators/01-ArithmaticOperators/03-IncrementDecrementOperators/IncrementDecrementOperators.c
+++ 08-C/07-Operators/01-ArithmaticOperators/03-IncrementDecrementOperators/IncrementDecrementOperators.c
@@ -1,22 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main(void)
 {
-	//variable declarations
-	int a = 3;
-	int b = 20;
+	int a = 5, b = 0;
 
-	//code
 	printf("\n\n");
 	printf("A = %d\n", a);
 	printf("A = %d\n", a++);
 	printf("A = %d\n", a);
-	printf("A = %d\n\n", ++a);
+	printf("A = %d\n", --a);
+	printf("A = %d\n", a);
+	printf("A = %d\n", ++a);
+	printf("A = %d\n", a);
+	printf("A = %d\n\n", a--);
 
+	printf("B = %d\n", b);
+	printf("B = %d\n", --b);
 	printf("B = %d\n", b);
 	printf("B = %d\n", b--);
 	printf("B = %d\n", b);
-	printf("B = %d\n\n", --b);
+	printf("B = %d\n", b++);
+	printf("B = %d\n", b);
+	printf("B = %d\n", ++b);
 
 	getch();
 	return(0);
