cba772dab4cc39790aa01f1ce342633f551af777
ShrutiKulkarni03 <shru3k@gmail.com>
2020-07-12 17:49:13 +0530

    Windows Window

---
+++ 01-Windows/00-Windowing/08-OpenGLTemplate/OpenGLTemplate.cpp
@@ -0,0 +1,229 @@
+//header files
+#include<windows.h>
+#include<stdio.h>
+#include "SPK.h"
+
+//macro functions
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+//global funtion declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void Initialize(void);
+void Display(void);
+void Uninitialize(void);
+
+//global variable declaration
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+bool gbFullscreen = false;
+HWND ghwnd = NULL;
+bool gbActiveWindow = false;
+FILE* gpFile = NULL;
+
+//WinMain
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+
+	//local variable declaration
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+	bool bDone = false;
+	int x, y, width, height;
+
+	//code
+	width = GetSystemMetrics(SM_CXSCREEN);
+	height = GetSystemMetrics(SM_CYSCREEN);
+
+	x = (width / 2) - 400;
+	y = (height / 2) - 300;
+
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+
+	RegisterClassEx(&wndclass);
+
+	//CreateWindow
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		szAppName,
+		TEXT("My Application - SHRUTI KULKARNI"),
+		(WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE),
+		100,
+		100,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+
+	//function call
+	Initialize();                      
+	ShowWindow(hwnd, iCmdShow);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+
+	//message loop
+	while (bDone == false)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				bDone = true;
+
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+
+		}
+		else
+		{
+			if (gbActiveWindow == true)
+			{
+				//update function for OpenGL rendering
+
+				//display function for OpenGL rendering
+				Display();
+
+			}
+		}
+
+	}
+		
+	return(msg.wParam);
+}
+
+//WndProc
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//function declaration
+	void ToggleFullscreen(void); 
+	void Resize(int, int);
+
+	switch (iMsg)
+	{
+	case WM_SETFOCUS:
+		gbActiveWindow = true;
+		break;
+
+	case WM_KILLFOCUS:
+		gbActiveWindow = false;
+		break;
+
+	case WM_ERASEBKGND:
+		return(0);
+
+	case WM_SIZE:
+		Resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hwnd);
+			break;
+
+		case 0x46:
+		case 0x66:
+			ToggleFullscreen();
+			break;
+		}
+		break;
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		Uninitialize();
+		PostQuitMessage(0);
+		break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void ToggleFullscreen(void)
+{
+	//local variable declaration
+	MONITORINFO mi = { sizeof(MONITORINFO) };
+
+	//code
+	if (gbFullscreen == false)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			if (GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, (dwStyle & ~WS_OVERLAPPEDWINDOW));
+
+				SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);
+			}
+
+		}
+		ShowCursor(false);
+		gbFullscreen = true;
+	}
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, (dwStyle | WS_OVERLAPPEDWINDOW));
+
+		SetWindowPlacement(ghwnd, &wpPrev);
+
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+
+		ShowCursor(true);
+		gbFullscreen = false;
+	}
+}
+
+void Initialize(void)
+{
+	//function prototype
+	void Resize(int, int);
+
+	//variable declaration
+
+
+	//code
+	Resize(WIN_WIDTH, WIN_HEIGHT);
+}
+
+void Resize(int width, int height)
+{
+	//code
+	if (height == 0)
+		height = 1;
+}
+void Display(void)
+{
+	//code
+}
+void Uninitialize(void)
+{
+	//code
+	if (gpFile)
+	{
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+}
