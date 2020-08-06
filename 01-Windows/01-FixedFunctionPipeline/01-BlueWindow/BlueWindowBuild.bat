036bfe2c0eb93bce516f7efd3c0bb2dd22463056
ShrutiKulkarni03 <shru3k@gmail.com>
2020-08-06 20:31:39 +0530

    Delete

--- 01-Windows/01-FixedFunctionPipeline/01-BlueWindow/BlueWindowBuild.bat
+++
@@ -1,7 +0,0 @@
-cl.exe /c /EHsc BlueWindow.cpp
-
-rc.exe MyIcon.rc
-
-link.exe BlueWindow.obj MyIcon.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS
-
-BlueWindow
--- 01-Windows/01-FixedFunctionPipeline/03-DoubleBuffer(Triangle)/Triangle_DoubleBuffer.bat
+++
@@ -1,9 +0,0 @@
-
-cl.exe /c /EHsc Triangle_DoubleBuffer.cpp
-
-//Resource Compile
-rc.exe Triangle_DoubleBuffer.res 
-
-link.exe Triangle_DoubleBuffer.obj Triangle_DoubleBuffer.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS
-
-Triangle_DoubleBuffer
