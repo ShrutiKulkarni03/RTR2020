d6160133f327b7d610ad502359160c28602f909d
ShrutiKulkarni03 <shru3k@gmail.com>
2020-09-17 00:39:04 +0530

    Windows Window

---
+++ 01-Windows/00-Windowing/06-Fullscreen/02-Method2/Fullscreen2.bat
@@ -0,0 +1,6 @@
+
+cl.exe /c /EHsc Fullscreen2.cpp
+
+link.exe Fullscreen2.obj user32.lib gdi32.lib /SUBSYSTEM:WINDOWS
+
+Fullscreen2
---
+++ 01-Windows/00-Windowing/06-Fullscreen/02-Method2/Fullscreen2.cpp
@@ -0,0 +1,153 @@
+#include<windows.h>
+#include<WinUser.h>
+
+//global funtion declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//global variable declaration
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+bool gbFullscreen = false;
+HWND ghwnd = NULL;
+
+//WinMain
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+
+	//variable declaration
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+	int x, y, width, height;
+
+	//code
+	width = GetSystemMetrics(SM_CXSCREEN);
+	height = GetSystemMetrics(SM_CYSCREEN);
+
+	x = (width / 2) - 400;
+	y = (height / 2) - 300;
+
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	RegisterClassEx(&wndclass);
+
+	//create window
+	hwnd = CreateWindow(szAppName,
+		TEXT("My Application - SHRUTI KULKARNI"),
+		WS_OVERLAPPEDWINDOW,
+		x,
+		y,
+		800,
+		600,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//message loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return((int)msg.wParam);
+}
+
+//WndProc
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	void ToggleFullscreen(void); //function declaration
+
+	switch (iMsg)
+	{
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hwnd);
+			break;
+
+		case 0x46:
+		case 0x66:
+			ToggleFullscreen();
+			break;
+		}
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void ToggleFullscreen(void)
+{
+	//local variable declaration
+	MONITORINFO mi = { sizeof(MONITORINFO) };
+	DEVMODE lpDevMode;
+	DWORD dwFlags;
+	LONG CDS;
+
+
+	//code
+	if (gbFullscreen == false)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			if (GetWindowPlacement(ghwnd, &wpPrev) && EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &lpDevMode), &mi)
+			{
+				lpDevMode.dmPelsWidth = 800;
+				lpDevMode.dmPelsHeight = 600;
+				lpDevMode.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT;
+
+				dwFlags = CDS_FULLSCREEN;
+
+				SetWindowLong(ghwnd, GWL_STYLE, (dwStyle & ~WS_OVERLAPPEDWINDOW));
+
+				//SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);
+
+				CDS = ChangeDisplaySettings(&lpDevMode, dwFlags);
+			}
+
+		}
+		ShowCursor(false);
+		gbFullscreen = true;
+	} 
+	else
+	{
+
+		CDS = ChangeDisplaySettings(NULL, 0);
+
+		SetWindowLong(ghwnd, GWL_STYLE, (dwStyle | WS_OVERLAPPEDWINDOW));		
+		
+		SetWindowPlacement(ghwnd, &wpPrev);		
+
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+	
+		ShowCursor(true);
+		gbFullscreen = false;
+	}
+}
+
