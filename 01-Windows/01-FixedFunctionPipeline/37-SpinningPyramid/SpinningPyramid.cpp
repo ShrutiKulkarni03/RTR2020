5da52502d68d79eb9cf25491fbc715da9a8aac99
ShrutiKulkarni03 <shru3k@gmail.com>
2020-09-22 10:17:53 +0530

    Windows FFP

--- 01-Windows/01-FixedFunctionPipeline/37-SpinningPyramid/SpinningPyramid.cpp
+++ 01-Windows/01-FixedFunctionPipeline/37-SpinningPyramid/SpinningPyramid.cpp
@@ -318,28 +318,32 @@ void Display(void)
 
 	glBegin(GL_TRIANGLES);
 
-	glColor3f(1.0f, 0.0f, 0.0f);
-
+	glColor3f(1.0f, 1.0f, 0.0f);
 	glVertex3f(0.0f, 0.5f, 0.0f);
-	glVertex3f(-0.5f, -0.5f, 0.5f);
-	glVertex3f(0.5f, -0.5f, 0.5f);
-	
-	glColor3f(0.0f, 1.0f, 0.0f);
-
-	glVertex3f(0.0f, 0.5f, 0.0f);
-	glVertex3f(0.5f, -0.5f, 0.5f);
-	glVertex3f(0.5f, -0.5f, -0.5f);
-	
-	glColor3f(0.0f, 0.0f, 1.0f);
-
-	glVertex3f(0.0f, 0.5f, 0.0f);
-	glVertex3f(0.5f, -0.5f, -0.5f);
-	glVertex3f(-0.5f, -0.5f, -0.5f);
-	
 	glColor3f(0.0f, 1.0f, 1.0f);
-
+	glVertex3f(-0.5f, -0.5f, 0.5f);
+	glColor3f(1.0f, 0.0f, 1.0f);
+	glVertex3f(0.5f, -0.5f, 0.5f);
+	
+	glColor3f(1.0f, 1.0f, 0.0f);
 	glVertex3f(0.0f, 0.5f, 0.0f);
+	glColor3f(1.0f, 0.0f, 1.0f);
+	glVertex3f(0.5f, -0.5f, 0.5f);
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glVertex3f(0.5f, -0.5f, -0.5f);
+	
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 0.5f, 0.0f);
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glVertex3f(0.5f, -0.5f, -0.5f);
+	glColor3f(1.0f, 0.0f, 1.0f);
 	glVertex3f(-0.5f, -0.5f, -0.5f);
+	
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 0.5f, 0.0f);
+	glColor3f(1.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, -0.5f, -0.5f);
+	glColor3f(0.0f, 1.0f, 1.0f);
 	glVertex3f(-0.5f, -0.5f, 0.5f);
 
 	glEnd();
