df5d8a24c6b068e255966ba480822f49ea5d2e03
ShrutiKulkarni03 <shru3k@gmail.com>
2020-12-13 12:12:45 +0530

    Windows FFP

--- 01-Windows/01-FixedFunctionPipeline/53-ThreeRotatingLightsOnSteadySphere/ThreeRotatingLightsOnSteadySphere.bat
+++
@@ -1,7 +0,0 @@
-cl.exe /c /EHsc ThreeRotatingLightsOnSteadySphere.cpp
-
-rc.exe MyIcon.rc
-
-link.exe ThreeRotatingLightsOnSteadySphere.obj MyIcon.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS
-
-ThreeRotatingLightsOnSteadySphere
