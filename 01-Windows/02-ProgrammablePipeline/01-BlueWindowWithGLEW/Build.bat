d1d24b14650fca0af681da73e8637fdd3cc8182a
ShrutiKulkarni03 <shru3k@gmail.com>
2021-03-04 00:10:36 +0530

    PP Blue Window

---
+++ 01-Windows/02-ProgrammablePipeline/01-BlueWindowWithGLEW/Build.bat
@@ -0,0 +1,7 @@
+cl.exe /c /EHsc /I C:\glew-2.1.0\include BlueWindow.cpp
+
+rc.exe Resources.rc
+
+link.exe BlueWindow.obj Resources.res user32.lib gdi32.lib /LIBPATH:"C:\glew-2.1.0\lib\Release\Win32" /SUBSYSTEM:WINDOWS 
+
+BlueWindow
