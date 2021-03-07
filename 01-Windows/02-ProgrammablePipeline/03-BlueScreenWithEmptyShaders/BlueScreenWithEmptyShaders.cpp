fdedcd0c2b0be97bd704b027149f92fcd3798c67
ShrutiKulkarni03 <shru3k@gmail.com>
2021-03-07 05:31:33 +0530

    Blue Screen With Pass-Through Shaders

---
+++ 01-Windows/02-ProgrammablePipeline/03-BlueScreenWithEmptyShaders/BlueScreenWithEmptyShaders.cpp
@@ -0,0 +1,468 @@
+//header files
+#include<windows.h>
+#include<stdio.h>
+
+#include<gl\glew.h>
+
+#include<GL/GL.h>
+#include "Resources.h"
+
+
+//macro functions
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "OpenGL32.lib")
+
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+//global function declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void Initialize(void);
+void Display(void);
+void Uninitialize(void);
+
+//global variable declaration
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+bool gbFullscreen = false;
+HWND ghwnd = NULL;
+bool gbActiveWindow = false;
+FILE* gpFile = NULL;
+HDC ghdc = NULL;
+HGLRC ghrc = NULL;
+
+GLuint vertexShaderObject;
+GLuint fragmentShaderObject;
+GLuint shaderProgramObject;
+
+//WinMain
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//local variable declaration
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+	bool bDone = false;
+	int x, y, width, height;
+
+	if (fopen_s(&gpFile, "RenderLog.txt", "w") != 0)
+	{
+		MessageBox(NULL, TEXT("Cannot Open The Desired File\n"), TEXT("Error"), MB_OK);
+		exit(0);
+	}
+	else
+	{
+		fprintf(gpFile, ("Log File Created Successfully, Program Started Successfully.\n\n"));
+	}
+
+	//code
+	width = GetSystemMetrics(SM_CXSCREEN);
+	height = GetSystemMetrics(SM_CYSCREEN);
+
+	x = (width / 2) - 400;
+	y = (height / 2) - 300;
+
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+
+	RegisterClassEx(&wndclass);
+
+	//CreateWindow
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		szAppName,
+		TEXT("My Application - SHRUTI KULKARNI"),
+		(WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE),
+		100,
+		100,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+
+	//funtion call
+	Initialize();
+	ShowWindow(hwnd, iCmdShow);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+
+	//game loop
+	while (bDone == false)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				bDone = true;
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if (gbActiveWindow == true)
+			{
+				//update function for OpenGL rendering
+
+				//display function for OpenGL rendering
+				Display();
+
+			}
+		}
+	}
+	return(msg.wParam);
+}
+
+//WndProc
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//function declaration
+	void ToggleFullscreen(void);
+	void Resize(int, int);
+
+	switch (iMsg)
+	{
+	case WM_SETFOCUS:
+		gbActiveWindow = true;
+		break;
+
+	case WM_KILLFOCUS:
+		gbActiveWindow = false;
+		break;
+
+	case WM_ERASEBKGND:
+		return(0);
+
+	case WM_SIZE:
+		Resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hwnd);
+			break;
+
+		case 0x46:
+		case 0x66:
+			ToggleFullscreen();
+			break;
+		}
+		break;
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		Uninitialize();
+		PostQuitMessage(0);
+		break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+
+}
+
+void ToggleFullscreen(void)
+{
+	//local variable declaration
+	MONITORINFO mi = { sizeof(MONITORINFO) };
+
+	//code
+	if (gbFullscreen == false)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			if (GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, (dwStyle & ~WS_OVERLAPPEDWINDOW));
+				SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);
+			}
+
+		}
+		ShowCursor(false);
+		gbFullscreen = true;
+	}
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, (dwStyle | WS_OVERLAPPEDWINDOW));
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+
+		ShowCursor(true);
+		gbFullscreen = false;
+	}
+}
+
+void Initialize(void)
+{
+	//function prototype
+	void Resize(int, int);
+
+	//variable declaration
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex;
+
+	//code
+	ghdc = GetDC(ghwnd);
+
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	//struct pfd
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 8;
+	pfd.cDepthBits = 32;
+
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+
+	if (iPixelFormatIndex == 0)
+	{
+		fprintf(gpFile, ("ChoosePixelFormat() Failed.\n"));
+		DestroyWindow(ghwnd);
+	}
+
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE)
+	{
+		fprintf(gpFile, ("SetPixelFormat() Failed.\n"));
+		DestroyWindow(ghwnd);
+	}
+
+	ghrc = wglCreateContext(ghdc);
+
+	if (ghrc == NULL)
+	{
+		fprintf(gpFile, ("wglCreateContext() Failed.\n"));
+		DestroyWindow(ghwnd);
+	}
+
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE)
+	{
+		fprintf(gpFile, ("wglMakeCurrent() Failed\n"));
+		DestroyWindow(ghwnd);
+	}
+
+	GLenum glew_error = glewInit();
+	if (glew_error != GLEW_OK)
+	{
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	//OpenGL related LOG
+
+	fprintf(gpFile, "OpenGL VENDOR : %s\n", glGetString(GL_VENDOR));
+	fprintf(gpFile, "OpenGL RENDERER : %s\n", glGetString(GL_RENDERER));
+	fprintf(gpFile, "OpenGL VERSION : %s\n", glGetString(GL_VERSION));
+	fprintf(gpFile, "GLSL[Graphics Library Shading Language] VERSION : %s\n\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+	fprintf(gpFile, "EXTENTIONS : \n");
+
+	//OpenGL Enabled Extensions
+
+	GLint numExt;
+
+	glGetIntegerv(GL_NUM_EXTENSIONS, &numExt);
+
+	for (int i = 0; i < numExt; i++)
+	{
+		fprintf(gpFile, "%s\n", glGetStringi(GL_EXTENSIONS, i));
+	}
+
+
+	/*****VERTEX SHADER*****/
+
+	//create shader
+	vertexShaderObject = glCreateShader(GL_VERTEX_SHADER);
+
+	//provide source code to vertex shader
+	const GLchar* vertexShaderSourceCode =
+		"#version 450" \
+		"\n" \
+		"void main(void)" \
+		"{" \
+		"}";
+
+	glShaderSource(vertexShaderObject, 1, (const GLchar**)&vertexShaderSourceCode, NULL);
+
+	//compile shader
+	glCompileShader(vertexShaderObject);
+
+	//vertex shader compilation error checking
+
+
+
+	/*****FRAGMENT SHADER*****/
+
+	//create shader
+	fragmentShaderObject = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//provide source code to fragment shader
+	const GLchar* fragmentShaderSourceCode =
+		"#version 450" \
+		"\n" \
+		"void main(void)" \
+		"{" \
+		"}";
+
+	glShaderSource(fragmentShaderObject, 1, (const GLchar**)&fragmentShaderSourceCode, NULL);
+
+	//compile shader
+	glCompileShader(fragmentShaderObject);
+
+	//fragment shader compilation error checking
+
+
+
+	/*****SHADER PROGRAM*****/
+
+	//create
+	shaderProgramObject = glCreateProgram();
+
+	//attach vertex shader to shader program
+	glAttachShader(shaderProgramObject, vertexShaderObject);
+
+	//attach fragment shader to shader program
+	glAttachShader(shaderProgramObject, fragmentShaderObject);
+
+	//link shader
+	glLinkProgram(shaderProgramObject);
+
+	//shader linking error checking
+
+	
+
+	//depth
+	glShadeModel(GL_SMOOTH);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glEnable(GL_CULL_FACE);
+
+	//SetClearColor
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
+	Resize(WIN_WIDTH, WIN_HEIGHT);
+}
+
+void Resize(int width, int height)
+{
+	//code
+	if (height == 0)
+		height = 1;
+
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+}
+
+void Display(void)
+{
+	//code
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//start using OpenGL program object
+	glUseProgram(shaderProgramObject);
+
+	//OpenGL Drawing
+
+	//stop using OpenGL program object
+	glUseProgram(0);
+
+
+	SwapBuffers(ghdc);
+}
+
+void Uninitialize(void)
+{
+	//code
+	if (gbFullscreen == true)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+
+		SetWindowLong(ghwnd, GWL_STYLE, (dwStyle | WS_OVERLAPPEDWINDOW));
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+
+		ShowCursor(true);
+	}
+
+	/*****SHADERS*****/
+
+	//detach vertex shader from shaderprogram object
+	glDetachShader(shaderProgramObject, vertexShaderObject);
+
+	//detach fragment shader from shaderprogram object
+	glDetachShader(shaderProgramObject, fragmentShaderObject);
+
+	//delete vertex shader object
+	glDeleteShader(vertexShaderObject);
+	vertexShaderObject = 0;
+
+	//delete fragment shader object
+	glDeleteShader(fragmentShaderObject);
+	fragmentShaderObject = 0;
+
+	//delete shader program object
+	glDeleteProgram(shaderProgramObject);
+	shaderProgramObject = 0;
+
+	//unlink shader program
+	glUseProgram(0);
+
+
+
+	//
+	if (wglGetCurrentContext() == ghrc)
+	{
+		wglMakeCurrent(NULL, NULL);
+	}
+
+	//
+	if (ghrc)
+	{
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+	}
+
+	//
+	if (ghdc)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	//
+	if (gpFile)
+	{
+		fprintf(gpFile, ("\nLog File Closed Successfully, Program Completed Successfully.\n"));
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+}
---
+++ 01-Windows/02-ProgrammablePipeline/03-BlueScreenWithEmptyShaders/Build.bat
@@ -0,0 +1,7 @@
+cl.exe /c /EHsc /I C:\glew-2.1.0\include BlueScreenWithEmptyShaders.cpp
+
+rc.exe Resources.rc
+
+link.exe BlueScreenWithEmptyShaders.obj Resources.res user32.lib gdi32.lib /LIBPATH:"C:\glew-2.1.0\lib\Release\Win32" /SUBSYSTEM:WINDOWS 
+
+BlueScreenWithEmptyShaders
