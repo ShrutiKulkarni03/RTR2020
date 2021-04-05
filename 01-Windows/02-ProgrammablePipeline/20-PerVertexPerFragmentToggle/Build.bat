0aa6948b908b09fb9a13930a35f69c267af9ea10
ShrutiKulkarni03 <shru3k@gmail.com>
2021-04-06 00:50:50 +0530

    PVLighting-PFLighting Toggle

---
+++ 01-Windows/02-ProgrammablePipeline/20-PerVertexPerFragmentToggle/Build.bat
@@ -0,0 +1,8 @@
+cl.exe /c /EHsc /I C:\glew-2.1.0\include PerVertexPerFragmentToggle.cpp
+
+rc.exe Resources.rc
+
+link.exe PerVertexPerFragmentToggle.obj Resources.res user32.lib gdi32.lib /LIBPATH:"C:\glew-2.1.0\lib\Release\Win32" /SUBSYSTEM:WINDOWS 
+
+PerVertexPerFragmentToggle
+
