aff1f3c42f0f9d83d0cfdf5efdee243f7d4d7eba
ShrutiKulkarni03 <shru3k@gmail.com>
2021-04-26 19:50:38 +0530

    Hello World

---
+++ 03-Android/00-Windowing/02-HelloWorld/app/src/main/java/com/example/hello/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+import android.graphics.Color;
+
+import android.view.Gravity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setBackgroundColor(Color.rgb(0, 0, 0));
+
+        AppCompatTextView myTextView = new AppCompatTextView(this);
+
+        myTextView.setText("Hello World!");
+        myTextView.setTextSize(32);
+        myTextView.setTextColor(Color.rgb(0, 255, 0));
+        myTextView.setGravity(Gravity.CENTER);
+        myTextView.setBackgroundColor(Color.rgb(0, 0, 0));
+
+        setContentView(myTextView);
+
+        System.out.println("SPK: Hello World");
+    }
+}
+
