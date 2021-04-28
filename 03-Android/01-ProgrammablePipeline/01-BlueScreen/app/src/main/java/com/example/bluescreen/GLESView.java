5b143f6d304bea721917238c02784f44842bcdfc
ShrutiKulkarni03 <shru3k@gmail.com>
2021-04-28 13:09:14 +0530

    BlueScreen

---
+++ 03-Android/01-ProgrammablePipeline/01-BlueScreen/app/src/main/java/com/example/bluescreen/GLESView.java
@@ -0,0 +1,164 @@
+package com.example.bluescreen;
+
+import android.content.Context;
+
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnDoubleTapListener;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
+
+import android.opengl.GLES32;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class GLESView extends GLSurfaceView implements GLSurfaceView.Renderer, OnGestureListener, OnDoubleTapListener
+{
+	private final Context context;
+	private GestureDetector gestureDetector;
+	
+	public GLESView(Context drawingContext)
+	{
+		super(drawingContext);
+
+		context = drawingContext;
+
+		setEGLContextClientVersion(3);
+
+		setRenderer(this);
+
+		setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+		gestureDetector = new GestureDetector(context, this, null, false);
+		gestureDetector.setOnDoubleTapListener(this);
+
+	}
+
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config)
+	{
+		String version = gl.glGetString(GL10.GL_VERSION);
+		System.out.println("SPK: "+version);
+
+		initialize(gl);
+	}
+
+
+	@Override
+	public void onSurfaceChanged(GL10 unused, int width, int height)
+	{
+		resize(width, height);
+	}
+
+
+	@Override
+	public void onDrawFrame(GL10 unused)
+	{
+		draw();
+	}
+
+
+	@Override
+	public boolean onTouchEvent(MotionEvent e)
+	{
+		int eventaction = e.getAction();
+		if(!gestureDetector.onTouchEvent(e))
+			super.onTouchEvent(e);
+
+		return(true);
+	}
+
+
+	@Override
+	public boolean onDoubleTap(MotionEvent e)
+	{
+		return(true);
+	}
+
+
+	@Override
+	public boolean onDoubleTapEvent(MotionEvent e)
+	{
+		return(true);
+	}
+
+
+	@Override
+	public boolean onSingleTapConfirmed(MotionEvent e)
+	{
+		return(true);
+	}
+
+
+	@Override
+	public boolean onDown(MotionEvent e)
+	{
+		return(true);
+	}
+
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+	{
+		return(true);
+	}
+
+
+	@Override
+	public void onLongPress(MotionEvent e)
+	{
+		//code
+	}
+
+
+	@Override
+	public void onShowPress(MotionEvent e)
+	{
+		//code
+	}
+
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
+	{
+		uninitialize();
+		System.exit(0);
+		return(true);
+	}
+
+
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e)
+	{
+		return(true);
+	}
+
+
+	private void initialize(GL10 gl)
+	{
+		GLES32.glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+	}
+
+
+	private void resize(int width, int height)
+	{
+		GLES32.glViewport(0, 0, width, height);
+	}
+
+
+	private void draw()
+	{
+		GLES32.glClear(GLES32.GL_COLOR_BUFFER_BIT | GLES32.GL_DEPTH_BUFFER_BIT);
+	}
+
+
+	private void uninitialize()
+	{
+		//code
+	}
+
+}
+
