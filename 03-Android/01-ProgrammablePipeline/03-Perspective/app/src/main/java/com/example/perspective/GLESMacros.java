6ce77dea24604c43d3929747f67c7b29c07381ba
ShrutiKulkarni03 <shru3k@gmail.com>
2021-05-10 11:49:38 +0530

    Perspective

---
+++ 03-Android/01-ProgrammablePipeline/03-Perspective/app/src/main/java/com/example/perspective/GLESMacros.java
@@ -0,0 +1,9 @@
+package com.example.perspective;
+
+public class GLESMacros
+{
+	public static final int SPK_ATTRIBUTE_POSITION = 0;
+	public static final int SPK_ATTRIBUTE_COLOR = 1;
+	public static final int SPK_ATTRIBUTE_NORMAL = 2;
+	public static final int SPK_ATTRIBUTE_TEXCOORD = 3;
+}
