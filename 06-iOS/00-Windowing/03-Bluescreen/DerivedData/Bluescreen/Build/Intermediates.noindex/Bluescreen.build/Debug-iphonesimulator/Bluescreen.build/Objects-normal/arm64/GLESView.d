6bc0c471e85af5bb073f77a72381270962799377
Shruti Kulkarni <shrutikulkarni@Shrutis-MacBook-Pro.local>
2021-07-11 02:49:02 +0530

    Deleted Dir

--- 06-iOS/00-Windowing/03-Bluescreen/DerivedData/Bluescreen/Build/Intermediates.noindex/Bluescreen.build/Debug-iphonesimulator/Bluescreen.build/Objects-normal/arm64/GLESView.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/shrutikulkarni/Astromedicomp/MyCodes/06-iOS/blue/Bluescreen/Bluescreen/GLESView.m \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/dyld.modulemap \
-  /Users/shrutikulkarni/Astromedicomp/MyCodes/06-iOS/blue/Bluescreen/Bluescreen/GLESView.h
