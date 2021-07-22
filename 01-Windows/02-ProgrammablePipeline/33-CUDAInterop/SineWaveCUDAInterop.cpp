01cfc83ffec510b50c82f5c794a8b9235518e698
ShrutiKulkarni03 <shru3k@gmail.com>
2021-07-22 23:17:43 +0530

    CUDA-OpenGL Interop_SineWave

--- 01-Windows/02-ProgrammablePipeline/33-CUDAInterop/SineWaveCUDAInterop.cpp
+++ 01-Windows/02-ProgrammablePipeline/33-CUDAInterop/SineWaveCUDAInterop.cpp
@@ -685,9 +685,9 @@ void Display(void)
 	glVertexAttribPointer(SPK_ATTRIBUTE_POSITION, 4, GL_FLOAT, GL_FALSE, 0, NULL);
 	glEnableVertexAttribArray(SPK_ATTRIBUTE_POSITION);
 	
-	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	glPointSize(2.0f);
+	//glPointSize(2.0f);
 	glDrawArrays(GL_POINTS, 0, meshWidth * meshHeight);
 
 	animationTime += 0.01f;	
