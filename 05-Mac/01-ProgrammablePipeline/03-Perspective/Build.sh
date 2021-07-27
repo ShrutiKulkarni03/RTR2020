a4e0e4e617b32275e7f0209a19e785e86df13014
Shruti Kulkarni <shrutikulkarni@Shrutis-MacBook-Pro.local>
2021-07-27 18:22:47 +0530

    Perspective

--- 05-Mac/01-ProgrammablePipeline/03-Perspective/Build.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p Perspective.app/Contents/MacOS
-
-clang++ -Wno-deprecated-declarations -o Perspective.app/Contents/MacOS/Perspective Perspective.mm -framework Cocoa -framework quartzCore -framework OpenGL
