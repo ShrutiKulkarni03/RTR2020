adbb57ea5a2d134e9ce9bdf54f48f6bf3d94f6dd
Shruti Kulkarni <shrutikulkarni@Shrutis-MacBook-Pro.local>
2021-07-27 18:24:41 +0530

    Perspective White Triangle

---
+++ 05-Mac/01-ProgrammablePipeline/03-PerspectiveWhiteTriangle/Build.sh
@@ -0,0 +1,3 @@
+mkdir -p Perspective.app/Contents/MacOS
+
+clang++ -Wno-deprecated-declarations -o Perspective.app/Contents/MacOS/Perspective Perspective.mm -framework Cocoa -framework quartzCore -framework OpenGL
