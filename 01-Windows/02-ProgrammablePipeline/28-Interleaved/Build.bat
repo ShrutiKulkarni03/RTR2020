96408746d2bd9c867895f62536e58d94037af14c
ShrutiKulkarni03 <shru3k@gmail.com>
2021-07-29 13:44:10 +0530

    Interleaved

--- 01-Windows/02-ProgrammablePipeline/28-Interleaved/Build.bat
+++
@@ -1,8 +0,0 @@
-cl.exe /c /EHsc /I C:\glew-2.1.0\include Interleaved.cpp
-
-rc.exe Resources.rc
-
-link.exe Interleaved.obj Resources.res user32.lib gdi32.lib /LIBPATH:"C:\glew-2.1.0\lib\Release\Win32" /SUBSYSTEM:WINDOWS 
-
-Interleaved
-
--- 01-Windows/02-ProgrammablePipeline/28-Interleaved/Resources.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define MYICON 310
-#define KUNDALI_BITMAP 312
