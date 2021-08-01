ef5a1d2f774f4a15df128b6308531683c9784557
ShrutiKulkarni03 <shru3k@gmail.com>
2021-08-01 19:20:27 +0530

    Bluescreen

--- 07-D3D11/01-Bluescreen/Build.bat
+++ 07-D3D11/01-Bluescreen/Build.bat
@@ -2,6 +2,6 @@ cl.exe /c /EHsc D3D11Bluescreen.cpp
 
 rc.exe Resources.rc
 
-link.exe D3D11Bluescreen.obj Resources.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS 
+link.exe D3D11Bluescreen.obj Resources.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS /MACHINE:X64
 
 D3D11Bluescreen
