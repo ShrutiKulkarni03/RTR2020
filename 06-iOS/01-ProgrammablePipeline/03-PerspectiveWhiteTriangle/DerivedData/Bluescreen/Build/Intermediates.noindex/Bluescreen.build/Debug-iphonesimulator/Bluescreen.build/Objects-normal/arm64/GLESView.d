0ee2fb994eb4453a209be343cc792a47f7d025ce
Shruti Kulkarni <shrutikulkarni@Shrutis-MacBook-Pro.local>
2021-08-06 17:25:17 +0530

    Perspective White Triangle

--- 06-iOS/01-ProgrammablePipeline/03-PerspectiveWhiteTriangle/DerivedData/Bluescreen/Build/Intermediates.noindex/Bluescreen.build/Debug-iphonesimulator/Bluescreen.build/Objects-normal/arm64/GLESView.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/shrutikulkarni/Astromedicomp/MyCodes/06-iOS/01-ProgrammablePipeline/03-PerspectiveWhiteTriangle/Bluescreen/GLESView.mm \
-  /Users/shrutikulkarni/Astromedicomp/MyCodes/06-iOS/01-ProgrammablePipeline/03-PerspectiveWhiteTriangle/Bluescreen/GLESView.h \
-  /Users/shrutikulkarni/Astromedicomp/MyCodes/06-iOS/01-ProgrammablePipeline/03-PerspectiveWhiteTriangle/Bluescreen/vmath.h
--- 06-iOS/01-ProgrammablePipeline/03-PerspectiveWhiteTriangle/DerivedData/Bluescreen/Build/Intermediates.noindex/Bluescreen.build/Debug-iphonesimulator/Bluescreen.build/Objects-normal/arm64/main.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/shrutikulkarni/Astromedicomp/MyCodes/06-iOS/01-ProgrammablePipeline/03-PerspectiveWhiteTriangle/Bluescreen/main.m \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/dyld.modulemap \
-  /Users/shrutikulkarni/Astromedicomp/MyCodes/06-iOS/01-ProgrammablePipeline/03-PerspectiveWhiteTriangle/Bluescreen/AppDelegate.h
