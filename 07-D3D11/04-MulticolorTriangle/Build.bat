aa9b088d4a3713404d0112f0d11a7fbcd1609164
ShrutiKulkarni03 <shru3k@gmail.com>
2021-08-14 19:51:54 +0530

    Multicolor Triangle

--- 07-D3D11/04-MulticolorTriangle/Build.bat
+++
@@ -1,7 +0,0 @@
-cl.exe /c /EHsc MulticolorTriangle.cpp
-
-rc.exe Resources.rc
-
-link.exe MulticolorTriangle.obj Resources.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS /MACHINE:X64
-
-MulticolorTriangle
