3b532e0543ea4f214103d24a1cfac173bf0e9195
ShrutiKulkarni03 <shru3k@gmail.com>
2021-08-26 20:01:15 +0530

    Solar System test

---
+++ 04-WebGL/01-ProgrammablePipeline/23-SolarSystem/Canvas.js
@@ -0,0 +1,422 @@
+//CODE
+
+
+//Global Variables
+var canvas = null;
+var gl = null;
+var canvas_original_width;
+var canvas_original_height;
+var bFullscreen = false;
+
+const WebGLMacros = 
+{
+	SPK_ATTRIBUTE_POSITION:0,
+	SPK_ATTRIBUTE_COLOR:1,
+	SPK_ATTRIBUTE_NORMAL:2,
+	SPK_ATTRIBUTE_TEXCOORD:3
+};
+
+var vertexShaderObject;
+var fragmentShaderObject;
+var shaderProgramObject;
+
+var sphere = null;
+
+var mvpUniform;
+
+var hour = 0;
+var day = 0;
+var year = 0;
+
+var stack = null;
+
+var perspectiveProjectionMatrix;
+
+var requestAnimationFrame = window.requestAnimationFrame || 
+							window.webkitRequestAnimationFrame ||
+							window.wozRequestAnimationFrame ||
+							window.oRequestAnimationFrame ||
+							window.msRequestAnimationFrame;
+
+var cancelAnimationFrame =  window.cancelAnimationFrame ||
+							window.webkitCancelRequestAnimationFrame || window.webkitCancelAnimationFrame ||
+							window.mozCancelRequestAnimationFrame || window.mozCancelAnimationFrame ||
+							window.oCancelRequestAnimationFrame || window.oCancelAnimationFrame ||
+							window.msCancelRequestAnimationFrame || window.msCancelAnimationFrame;
+
+
+function main()
+{
+	//Get canvas from dom
+	canvas = document.getElementById("SPK");
+
+	if(!canvas)
+	{
+		console.log("Obtaining Canvas Failed. \n");
+	}
+	else
+	{
+		console.log("Obtaining Canvas Successful. \n");
+	}
+
+	canvas_original_width = canvas.width;
+	canvas_original_height = canvas.height;
+
+
+	window.addEventListener("keydown", keyDown, false);
+	window.addEventListener("click", mouseDown, false);
+
+	window.addEventListener("resize", resize, false);
+
+	init();
+
+	resize();    //warm-up resize
+
+	draw();      //warm-up draw
+}
+
+function toggleFullscreen()
+{
+	var fullscreen_element = document.fullscreenElement ||             
+							 document.webkitFullscreenElement ||      
+							 document.mozFullScreenElement ||
+							 document.msFullscreenElement ||
+							 null;
+
+
+	if(fullscreen_element == null)
+	{
+		if(canvas.requestFullscreen)
+			canvas.requestFullscreen();
+
+		else if(canvas.webkitRequestFullscreen)
+			canvas.webkitRequestFullscreen();
+
+		else if(canvas.mozRequestFullscreen)
+			canvas.mozRequestFullScreen();
+
+		else if(canvas.msRequestFullscreen)
+			msRequestFullscreen();
+
+		bFullscreen = true;
+	}
+
+	else
+	{
+		if(document.exitFullscreen)
+			document.exitFullscreen();
+
+		else if(document.webkitExitFullscreen)
+			document.webkitExitFullscreen();
+
+		else if(document.mozCancelFullScreen)
+			document.mozCancelFullScreen();
+
+		else if(document.msExitFullscreen)
+			document.msExitFullscreen();
+
+		bFullscreen = false;
+	}
+}
+
+function init()
+{
+	//get drawing context from the canvas
+	gl = canvas.getContext("webgl2");
+
+	if(!gl)
+	{
+		console.log("WebGL Context Obtaining Failed. \n");
+	}
+	else
+	{
+		console.log("WebGL Context Obtained Successful. \n");
+	}
+
+	gl.viewportWidth = canvas.width;
+	gl.viewportHeight = canvas.height;
+
+	//VERTEX SHADER
+
+	var vertexShaderSourceCode = 
+	"#version 300 es \n"+
+	"\n"+
+	"in vec4 vPosition; \n" +
+	"in vec4 vColor; \n" +
+	"uniform mat4 u_mvp_matrix; \n" +
+	"out vec4 out_color; \n" +
+	"void main(void) \n"+
+	"{ \n"+
+	"	gl_Position = u_mvp_matrix * vPosition; \n" +
+	"	out_color = vColor; \n" +
+	"} \n"
+
+	vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vertexShaderObject, vertexShaderSourceCode);
+	gl.compileShader(vertexShaderObject);
+
+	if(gl.getShaderParameter(vertexShaderObject, gl.COMPILE_STATUS)==false)
+	{
+		var error = gl.getShaderInfoLog(vertexShaderObject);
+		if(error.length > 0)
+		{
+			alert(error);
+			uinitialize();
+		}
+	}
+
+	//FRAGMENT SHADER
+
+	var fragmentShaderSourceCode = 
+	"#version 300 es \n"+
+	"\n"+
+	"precision highp float; \n" +
+	"in vec4 out_color; \n" +
+	"out vec4 FragColor; \n"+
+	"void main(void) \n"+
+	"{ \n"+
+	"	FragColor = out_color; \n" +
+	"} \n"
+
+	fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragmentShaderObject, fragmentShaderSourceCode);
+	gl.compileShader(fragmentShaderObject);
+
+	if(gl.getShaderParameter(fragmentShaderObject, gl.COMPILE_STATUS)==false)
+	{
+		var error = gl.getShaderInfoLog(fragmentShaderObject);
+		if(error.length > 0)
+		{
+			alert(error);
+			uinitialize();
+		}
+	}
+
+
+	//SHADER PROGRAM
+	shaderProgramObject = gl.createProgram();
+	gl.attachShader(shaderProgramObject, vertexShaderObject);
+	gl.attachShader(shaderProgramObject, fragmentShaderObject);
+	
+	//BINDING OF SHADER PROGRAM
+
+	gl.bindAttribLocation(shaderProgramObject, WebGLMacros.SPK_ATTRIBUTE_POSITION, "vPosition");
+	gl.bindAttribLocation(shaderProgramObject, WebGLMacros.SPK_ATTRIBUTE_COLOR, "vColor");
+	
+	gl.linkProgram(shaderProgramObject);
+
+	if(!gl.getProgramParameter(shaderProgramObject, gl.LINK_STATUS))
+	{
+		var error = gl.getShaderInfoLog(shaderProgramObject);
+		if(error.length > 0)
+		{
+			alert(error);
+			uinitialize();
+		}
+	}
+
+	//GET UNIFORM LOCATION
+
+	mvpUniform = gl.getUniformLocation(shaderProgramObject, "u_mvp_matrix");
+
+	//SPHERE
+
+	sphere = new Mesh();
+	makeSphere(sphere, 1.0, 30, 30);
+
+	//stack
+	stack = new Stack();
+	
+
+	//depth
+	gl.enable(gl.DEPTH_TEST);
+	gl.depthFunc(gl.LEQUAL);
+	gl.enable(gl.CULL_FACE);
+
+	//gl.polygonMode(gl.FRONT_AND_BACK, gl.LINE);
+
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+	perspectiveProjectionMatrix = mat4.create();
+
+}
+
+function resize()
+{
+	if(bFullscreen == true)
+	{
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+	}
+	else
+	{
+		canvas.width = canvas_original_width;
+		canvas.height = canvas_original_height;
+	}
+
+	gl.viewport(0, 0, canvas.width, canvas.height);
+
+	mat4.perspective(perspectiveProjectionMatrix, 45.0, parseFloat(canvas.width) / parseFloat(canvas.height), 0.1, 100.0);
+		
+}
+
+function draw()
+{
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	gl.useProgram(shaderProgramObject);
+
+	var modelViewMatrix = mat4.create();
+	var modelViewProjectionMatrix = mat4.create();
+
+	//SUN
+
+	mat4.translate(modelViewMatrix, modelViewMatrix, [0.0, 0.0, -6.0]);
+	//stack.push(modelViewMatrix);
+
+	//mat4.rotateX(modelViewMatrix, modelViewMatrix, degToRad(90.0));
+
+	mat4.multiply(modelViewProjectionMatrix, perspectiveProjectionMatrix, modelViewMatrix);
+	gl.uniformMatrix4fv(mvpUniform, false, modelViewProjectionMatrix);
+	gl.vertexAttrib3f(WebGLMacros.SPK_ATTRIBUTE_COLOR, 1.0, 0.5, 0.0);
+
+	sphere.draw();
+
+	//modelViewMatrix = stack.pop();
+	//stack.push(modelViewMatrix);
+
+
+	//EARTH
+
+	mat4.rotateY(modelViewMatrix, modelViewMatrix, degToRad(parseFloat(year)));
+	mat4.translate(modelViewMatrix, modelViewMatrix, [2.5, 0.0, 0.0]);
+	mat4.scale(modelViewMatrix, modelViewMatrix, [0.3, 0.3, 0.3]);
+	//mat4.rotateX(modelViewMatrix, modelViewMatrix, degToRad(90.0));
+
+	//stack.push(modelViewMatrix);
+
+	mat4.rotateY(modelViewMatrix, modelViewMatrix, degToRad(parseFloat(day)));
+
+	mat4.multiply(modelViewProjectionMatrix, perspectiveProjectionMatrix, modelViewMatrix);
+	gl.uniformMatrix4fv(mvpUniform, false, modelViewProjectionMatrix);
+	gl.vertexAttrib3f(WebGLMacros.SPK_ATTRIBUTE_COLOR, 0.4, 0.9, 1.0);
+
+	sphere.draw();
+
+	//modelViewMatrix = stack.pop();
+	//stack.push(modelViewMatrix);
+
+	//MOON
+
+	mat4.rotateY(modelViewMatrix, modelViewMatrix, degToRad(parseFloat(day)));
+	mat4.translate(modelViewMatrix, modelViewMatrix, [3.0, 0.0, 0.0]);
+	mat4.scale(modelViewMatrix, modelViewMatrix, [0.5, 0.5, 0.5]);
+
+	mat4.rotateY(modelViewMatrix, modelViewMatrix, degToRad(parseFloat(hour)));
+
+	mat4.multiply(modelViewProjectionMatrix, perspectiveProjectionMatrix, modelViewMatrix);
+	gl.uniformMatrix4fv(mvpUniform, false, modelViewProjectionMatrix);
+	gl.vertexAttrib3f(WebGLMacros.SPK_ATTRIBUTE_COLOR, 1.0, 1.0, 1.0);
+
+	sphere.draw();
+
+	//modelViewMatrix = stack.pop();
+	
+	gl.useProgram(null);
+
+	update();
+
+	requestAnimationFrame(draw);
+}
+
+function degToRad(degrees)
+{
+	return(degrees * Math.PI / 180.0);
+}
+
+function update()
+{
+	
+
+}
+
+function uninitialize()
+{
+	if(sphere)
+	{
+		sphere.deallocate();
+	}
+
+
+	if(shaderProgramObject)
+	{
+		if(fragmentShaderObject)
+		{
+			gl.detachShader(shaderProgramObject, fragmentShaderObject);
+			gl.deleteShader(fragmentShaderObject);
+			fragmentShaderObject = null;
+		}
+
+		if(vertexShaderObject)
+		{
+			gl.detachShader(shaderProgramObject, vertexShaderObject);
+			gl.deleteShader(vertexShaderObject);
+			vertexShaderObject = null;
+		}
+
+		gl.deleteProgram(shaderProgramObject);
+		shaderProgramObject = null;
+	}
+}
+
+
+function keyDown(event)
+{
+	switch(event.keyCode)
+	{
+		case 27:
+			uninitialize();
+			window.close();
+			break;
+		
+		case 70:
+			toggleFullscreen();
+			break;
+
+		case 72:
+			hour = (hour + 9) % 360;
+			break;
+
+		//case 'h':
+		//	hour = (hour - 9) % 360;
+		//	break;
+
+		case 68:
+			day = (day + 6) % 360;
+			hour = (hour + 9) % 360;
+			break;
+
+		//case 'd':
+		//	day = (day - 6) % 360;
+		//	hour = (hour - 9) % 360;
+		//	break;
+
+		case 89:
+			year = (year + 3) % 360;
+			break;
+
+		//case 'y':
+		//	year = (year - 3) % 360;
+		//	break;
+
+	}
+}
+
+function mouseDown()
+{
+	//code	
+}
+
+
+
+
---
+++ 04-WebGL/01-ProgrammablePipeline/23-SolarSystem/Stack.js
@@ -0,0 +1,21 @@
+// functions use without 'this' prefix are private to class.
+// functions with 'this' prefix are 'public' and hence when they use
+function Stack()
+{
+    var elements=[];
+    var top=0;
+
+    this.push=function(matrix)
+    {
+        // code
+        elements[top] = matrix;
+        top = top + 1;
+    }
+    
+    this.pop=function()
+    {
+        top = top - 1;
+        return elements.pop();
+    }
+   
+}
