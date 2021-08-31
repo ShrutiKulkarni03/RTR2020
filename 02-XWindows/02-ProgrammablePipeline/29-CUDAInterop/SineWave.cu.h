231344705412ae6dbcad5ef72a59a9f7acb22762
ShrutiKulkarni03 <shru3k@gmail.com>
2021-08-31 12:49:53 +0530

    HPP

--- 02-XWindows/02-ProgrammablePipeline/29-CUDAInterop/SineWave.cu.h
+++
@@ -1,4 +0,0 @@
-//function prototype
-void launchCUDAKernel(float4*, unsigned int, unsigned int, float);
-
-
--- 02-XWindows/02-ProgrammablePipeline/30-OpenCLInterop/build.sh
+++
@@ -1,4 +0,0 @@
-g++ -c -o OpenCLInterop.obj OpenCLInterop.cpp
-g++ -o OpenCLInterop OpenCLInterop.obj -lX11 -lGL -lGLEW -lOpenCL
-./OpenCLInterop
-
